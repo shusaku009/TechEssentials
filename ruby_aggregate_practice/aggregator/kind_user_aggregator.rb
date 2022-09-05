b4032086847b9af4f111690c59bffc23574b375a
shusaku009 <shusaku009@gmail.com>
2022-09-05 22:00:54 +0900

    aggregator/フォルダを修正

--- ruby_aggregate_practice/aggregator/kind_user_aggregator.rb
+++ ruby_aggregate_practice/aggregator/kind_user_aggregator.rb
@@ -7,6 +7,7 @@ class KindUserAggregator
     @channel_names = channel_names
   end
 
+  # 実装してください
   def exec
     times_reactions = @channel_names.map do |channel_name|
       #jsonファイルを読み込みんで変数に代入
@@ -17,7 +18,21 @@ class KindUserAggregator
   end
 
   def load(channel_name)
-    file = File.open(channel_name)
+    dir = File.expand_path("../data/#{channel_name}", File.dirname(__FILE__))
+    file = File.open(dir)
     JSON.load(file)
-  end
 end
+
+
+        #変数message内の
+        reactions = message["reactions"]
+        #変数"message"の要素数を調べる
+        reaction_count = reactions.length
+        #出力するためのハッシュを作成
+        {
+          channel_name: channel_name,
+          reaction_count: reaction_count
+        }
+      end
+      #変数execの配列を多い順に並べる
+        times_post.max_by(3) {|x| x[:reaction_count]}
