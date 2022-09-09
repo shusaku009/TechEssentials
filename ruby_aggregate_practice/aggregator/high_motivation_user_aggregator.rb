0c1afb8a623cb46cf9471c3422c7afc17afac4d7
shusaku009 <shusaku009@gmail.com>
2022-09-09 14:49:47 +0900

    【応用】Slackのデータ分析処理のファイルを保存

--- ruby_aggregate_practice/aggregator/high_motivation_user_aggregator.rb
+++ ruby_aggregate_practice/aggregator/high_motivation_user_aggregator.rb
@@ -9,11 +9,11 @@ class HighMotivationUserAggregator
 
   # 実装してください
   def exec
-    times_post = @channel_names.map do |channel_name|
+    times_post = channel_names.map do |channel_name|
       #jsonファイルを読み込みんで変数に代入
       channel = load(channel_name)
       #jsonファイルの変数"messages"を呼び出し変数"message"に代入
-      message = channel["messages"]
+      message = channel['messages']
       #変数"message"の要素数を調べる
       message_count = message.length
       #出力するためのハッシュを作成
@@ -25,6 +25,7 @@ class HighMotivationUserAggregator
     #変数execの配列を多い順に並べる
       times_post.max_by(3) {|x| x[:message_count]}
   end
+
 
   def load(channel_name)
     dir = File.expand_path("../data/#{channel_name}", File.dirname(__FILE__))
--- ruby_aggregate_practice/aggregator/kind_user_aggregator.rb
+++ ruby_aggregate_practice/aggregator/kind_user_aggregator.rb
@@ -1,3 +1,4 @@
+require 'debug'
 require 'json'
 
 class KindUserAggregator
@@ -7,32 +8,38 @@ class KindUserAggregator
     @channel_names = channel_names
   end
 
-  # 実装してください
+   # 実装してください
   def exec
-    times_reactions = @channel_names.map do |channel_name|
-      #jsonファイルを読み込みんで変数に代入
-      channel = load(channel_name)
-      #jsonファイルのキー"messages"を呼び出し変数"reactions"に代入
-      reactions = channel[:messages][:reactions]
+    #すべてのjsonファイルを繰り返し処理で読み込みんで変数に代入
+    channel_list = channel_names.map do |channel_name|
+      load(channel_name)
     end
-  end
 
+    times_reactions = channel_list.flat_map do |item|
+      #jsonファイルの配列'messages'を変数''messages'に代入
+      messages = item['messages']
+      #'messages'の配列の中に'reactions'の配列があれば変数'message_reactionsに代入
+      message_reactions = messages.filter { |message| !message['reactions'].nil?}
+      #'reactions'の配列がネストされているため、'flat_map'メソッドにてフラットな配列に変更
+      reactions = message_reactions.flat_map { |message| message['reactions'] }
+      #'users'の配列についても上記と同様に'flat_map'にてフラットな配列に変更し変数'reaction_users'に代入
+      reaction_users = reactions.flat_map { |reaction| reaction['users'] }
+      #binding.break
+    end
+    #binding.break
+    #変数'times_reactions'の配列を'group_by(&:itself)'にてuser_ie毎に出現回数を調べハッシュに変換。valueの数量を比較し、多い順3つに並べる。
+    times_reactions.group_by(&:itself).map{ |k, v| [k,v.size] }.to_h.max_by(3) {|_, v| v}.map do |user_id_and_reaction_count|
+      {
+        user_id: user_id_and_reaction_count.first,
+        reaction_count: user_id_and_reaction_count.last
+      }
+      # binding.break
+    end
+    # binding.break
+  end
   def load(channel_name)
     dir = File.expand_path("../data/#{channel_name}", File.dirname(__FILE__))
     file = File.open(dir)
     JSON.load(file)
+  end
 end
-
-
-        #変数message内の
-        reactions = message["reactions"]
-        #変数"message"の要素数を調べる
-        reaction_count = reactions.length
-        #出力するためのハッシュを作成
-        {
-          channel_name: channel_name,
-          reaction_count: reaction_count
-        }
-      end
-      #変数execの配列を多い順に並べる
-        times_post.max_by(3) {|x| x[:reaction_count]}
